360d0ad958bb569ff52e32dc06a0be987fc4a094
DeeBest <simphiwedladla8@gmail.com>
2024-06-18 10:20:29 +0200

    created footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import './../styles/footer.css';
+import { Link } from 'react-router-dom';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div className="icons-container">
+        <Link to="https://github.com/DeeBest">
+          <FaGithub className="icon" />
+        </Link>
+        <Link to="mailto:simphiwedladla8@gmail.com">
+          <FaEnvelope className="icon" />
+        </Link>
+        <Link to="https://www.linkedin.com/in/dladla-simphiwe-89061a20a/">
+          <FaLinkedin className="icon" />
+        </Link>
+      </div>
+      <small>Created By &copy;Simphiwe {currentYear}.</small>
+    </footer>
+  );
 };
 
 export default Footer;
