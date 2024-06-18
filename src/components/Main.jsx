e71e7637dd28b4842e7aa29e6a33192507574d9e
DeeBest <simphiwedladla8@gmail.com>
2024-06-18 08:27:56 +0200

    added styling to the subscription section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,12 @@ import PopularWomen from './PopularWomen';
 import PopularMen from './PopularMen';
 import PopularJewelry from './PopularJewelry';
 import PopularElectronics from './PopularElectronics';
+import { HiOutlineShoppingBag } from 'react-icons/hi2';
+import { FaFacebookF } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Main = ({ products }) => {
   return (
@@ -18,10 +24,43 @@ const Main = ({ products }) => {
           <button className="hero-btn">Shop Now</button>
         </section>
       </div>
-      <PopularWomen products={products} />
-      <PopularMen products={products} />
-      <PopularJewelry products={products} />
-      <PopularElectronics products={products} />
+      <div className="popular-section">
+        <PopularWomen products={products} />
+        <PopularMen products={products} />
+        <PopularJewelry products={products} />
+        <PopularElectronics products={products} />
+      </div>
+      <div className="subscription-section hero">
+        <h1>Get Exclusive Offers Directly In Your Email.</h1>
+        <p>Subscribe to our newsletter and stay updated.</p>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            value={(e) => e.target.value}
+          />
+          <button className="subscribe-btn" type="button">
+            Subscribe
+          </button>
+        </form>
+        <div className="logo-container">
+          <HiOutlineShoppingBag className="header-icon logo-icon" />
+          <h1>DeeBest Shop</h1>
+        </div>
+        <div className="links-container">
+          <Link>Company</Link>
+          <Link>Products</Link>
+          <Link>Offices</Link>
+          <Link>About</Link>
+          <Link>Contact</Link>
+        </div>
+        <div className="social-icons">
+          <FaFacebookF />
+          <FaInstagram />
+          <FaLinkedinIn />
+          <FaTwitter />
+        </div>
+      </div>
     </main>
   );
 };
