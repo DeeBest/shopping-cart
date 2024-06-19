5d4df876b3a6e6b0c32631ec5885843ffc3512cc
DeeBest <simphiwedladla8@gmail.com>
2024-06-19 22:38:58 +0200

    created spinner component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,29 +3,50 @@ import { useState, useEffect } from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Spinner from '../components/Spinner';
 
 const MainLayout = () => {
+  const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
   const [originalProducts, setOriginalProducts] = useState([]);
   const [cartItemsCounter, setCartItemsCounter] = useState(0);
 
   useEffect(() => {
-    const productsFetch = async () => {
-      const response = await (
-        await fetch('https://fakestoreapi.com/products')
-      ).json();
-      setProducts(response);
-      setOriginalProducts(response); // Keep the original products
-    };
-
-    productsFetch();
+    try {
+      const productsFetch = async () => {
+        const response = await (
+          await fetch('https://fakestoreapi.com/products')
+        ).json();
+        setProducts(response);
+        setOriginalProducts(response); // Keep the original products
+      };
+      productsFetch();
+    } catch (error) {
+      console.log('error fetching data', error);
+    } finally {
+      setLoading(false);
+    }
   }, []);
 
   return (
     <>
-      <Header cartItemsCounter={cartItemsCounter} />
-      <Outlet context={[products, setProducts, originalProducts]} />
-      <Footer />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Header cartItemsCounter={cartItemsCounter} />
+          <Outlet
+            context={[
+              products,
+              setProducts,
+              originalProducts,
+              loading,
+              setLoading,
+            ]}
+          />
+          <Footer />
+        </>
+      )}
     </>
   );
 };
