3556892c7003925d13e7bfcd2698b858bfe3b97b
DeeBest <simphiwedladla8@gmail.com>
2024-06-22 14:34:45 +0200

    added styling to the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,16 +1,78 @@
 import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react';
 
 const Cart = () => {
   const { productsInCart } = useOutletContext();
+
+  const [itemsCounter, setItemsCounter] = useState(1);
+  // const [priceUpdate, setPriceUpdate] = useState(Number(price));
+
+  const increaseQuantity = () => {
+    setItemsCounter((prev) => {
+      const newCounter = prev + 1;
+      // setPriceUpdate(Number(price) * newCounter);
+      return newCounter;
+    });
+  };
+
+  const decreaseQuantity = () => {
+    if (itemsCounter > 1) {
+      setItemsCounter((prev) => {
+        const newCounter = prev - 1;
+        // setPriceUpdate(Number(price) * newCounter);
+        return newCounter;
+      });
+    }
+  };
+
   return (
-    <main>
+    <main className="cart">
       <h1>Cart</h1>
       <hr />
-      <div className="cart-container">
-        {productsInCart.map((item) => {
-          return <p key={item.id}>{item.title}</p>;
-        })}
-      </div>
+      <section>
+        <div className="cart-items-container">
+          {productsInCart.map((item) => {
+            return (
+              <div key={item.id} className="cart-item">
+                <div className="cart-item-img-container">
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div className="title-btns-price-container">
+                  <h4>{item.title}</h4>
+                  <div className="quantity-container">
+                    <p className="product-price">
+                      Item(s) Price: ${item.price}
+                    </p>
+                    <div>
+                      <button
+                        className="quantity-btn decrease-quantity-btn"
+                        onClick={() => decreaseQuantity()}
+                      >
+                        -
+                      </button>
+                      <span>{itemsCounter}</span>
+                      <button
+                        className="quantity-btn increase-quantity-btn"
+                        onClick={() => increaseQuantity()}
+                      >
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <button className="del-item-in-cart-btn">Delete Item</button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="clear-cart-container">
+          <div>
+            <p>Total Costs:</p>
+            <p>$300</p>
+          </div>
+          <button>Clear Cart</button>
+        </div>
+      </section>
     </main>
   );
 };
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,30 +1,29 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
+// import { useState } from 'react';
 import { useOutletContext } from 'react-router-dom';
 
 const ProductCard = ({ isHome = false, id, imgUrl, title, price, product }) => {
-  const { setCartItemsCounter, productsInCart, setProductsInCart } =
-    useOutletContext();
-  const [itemsCounter, setItemsCounter] = useState(1);
-  const [priceUpdate, setPriceUpdate] = useState(Number(price));
+  const { setCartItemsCounter, setProductsInCart } = useOutletContext();
+  // const [itemsCounter, setItemsCounter] = useState(1);
+  // const [priceUpdate, setPriceUpdate] = useState(Number(price));
 
-  const increaseQuantity = () => {
-    setItemsCounter((prev) => {
-      const newCounter = prev + 1;
-      setPriceUpdate(Number(price) * newCounter);
-      return newCounter;
-    });
-  };
+  // const increaseQuantity = () => {
+  //   setItemsCounter((prev) => {
+  //     const newCounter = prev + 1;
+  //     setPriceUpdate(Number(price) * newCounter);
+  //     return newCounter;
+  //   });
+  // };
 
-  const decreaseQuantity = () => {
-    if (itemsCounter > 1) {
-      setItemsCounter((prev) => {
-        const newCounter = prev - 1;
-        setPriceUpdate(Number(price) * newCounter);
-        return newCounter;
-      });
-    }
-  };
+  // const decreaseQuantity = () => {
+  //   if (itemsCounter > 1) {
+  //     setItemsCounter((prev) => {
+  //       const newCounter = prev - 1;
+  //       setPriceUpdate(Number(price) * newCounter);
+  //       return newCounter;
+  //     });
+  //   }
+  // };
 
   const addProductToCart = (item) => {
     setProductsInCart((prevProducts) => {
@@ -42,8 +41,9 @@ const ProductCard = ({ isHome = false, id, imgUrl, title, price, product }) => {
       <div className="item-title-container">
         <p>{title}</p>
         <div className="price-quantity-container">
-          <p className="product-price">${priceUpdate.toFixed(2)}</p>
-          {isHome ? null : (
+          {/* <p className="product-price">${priceUpdate.toFixed(2)}</p> */}
+          <p className="product-price">${price}</p>
+          {/* {isHome ? null : (
             <div className="quantity-container">
               <button
                 className="quantity-btn decrease-quantity-btn"
@@ -59,7 +59,7 @@ const ProductCard = ({ isHome = false, id, imgUrl, title, price, product }) => {
                 +
               </button>
             </div>
-          )}
+          )} */}
         </div>
       </div>
       {isHome ? null : (
