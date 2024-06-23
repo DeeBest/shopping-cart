11dc4b8441b732d8baea6b8b6380036c8064bd01
DeeBest <simphiwedladla8@gmail.com>
2024-06-23 16:36:03 +0200

    modified the shop items align

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        injectData: {
+          base: '/shopping-cart/',
+        },
+      },
+    }),
+  ],
   base: '/shopping-cart/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
