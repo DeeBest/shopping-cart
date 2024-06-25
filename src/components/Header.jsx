3c3e2e24d5c918a185eaeef5e63bb87b0e244bbf
DeeBest <simphiwedladla8@gmail.com>
2024-06-25 12:17:11 +0200

    added mobile navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,14 @@ import { PiShoppingCart } from 'react-icons/pi';
 import { HiOutlineShoppingBag } from 'react-icons/hi2';
 import { NavLink } from 'react-router-dom';
 import DarkMode from './DarkMode';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
 
 const Header = ({ cartItemsCounter, isDarkMode, setIsDarkMode }) => {
+  const [isMobile, setIsMobile] = useState(true);
+
+  const toggleIsMobile = () => setIsMobile(!isMobile);
+
   const handleToggleDarkMode = (isDarkMode) => {
     setIsDarkMode(isDarkMode);
   };
@@ -16,8 +22,31 @@ const Header = ({ cartItemsCounter, isDarkMode, setIsDarkMode }) => {
         <HiOutlineShoppingBag className="header-icon logo-icon" />
         <h1>DeeBest Shop</h1>
       </div>
+      <div className="cart-container">
+        <span className="item-counter">{cartItemsCounter}</span>
+        <NavLink
+          className={({ isActive }) =>
+            `${isActive ? 'active' : ''} link ${
+              isDarkMode ? 'dark-mode-link' : ''
+            }`
+          }
+          to="cart"
+        >
+          <PiShoppingCart className="header-icon" />
+        </NavLink>
+      </div>
+      <DarkMode
+        isDarkMode={isDarkMode}
+        setIsDarkMode={setIsDarkMode}
+        handleToggleDarkMode={handleToggleDarkMode}
+      />
       <nav>
-        <ul>
+        <ul
+          className={`${isMobile ? 'show-nav desktop-nav' : 'desktop-nav'}`}
+          style={{
+            backgroundColor: `${isDarkMode ? '#1b1918' : '#e7e5e4'}`,
+          }}
+        >
           <li>
             <NavLink
               className={({ isActive }) =>
@@ -55,25 +84,10 @@ const Header = ({ cartItemsCounter, isDarkMode, setIsDarkMode }) => {
             </NavLink>
           </li>
         </ul>
+        <div className="mobile-navbar-icons" onClick={toggleIsMobile}>
+          {isMobile ? <FaTimes /> : <FaBars />}
+        </div>
       </nav>
-      <DarkMode
-        isDarkMode={isDarkMode}
-        setIsDarkMode={setIsDarkMode}
-        handleToggleDarkMode={handleToggleDarkMode}
-      />
-      <div className="cart-container">
-        <span className="item-counter">{cartItemsCounter}</span>
-        <NavLink
-          className={({ isActive }) =>
-            `${isActive ? 'active' : ''} link ${
-              isDarkMode ? 'dark-mode-link' : ''
-            }`
-          }
-          to="cart"
-        >
-          <PiShoppingCart className="header-icon" />
-        </NavLink>
-      </div>
     </header>
   );
 };
